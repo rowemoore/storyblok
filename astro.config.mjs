ff03e174cae8c2a4d75c9f1c297f339ef71d26c5
William Brugger <rowemoore@users.noreply.github.com>
2023-03-08 12:55:48 -0800

    sstoryblok setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  integrations: [
+    storyblok({
+      accessToken: env.Am3l4KqdK6p5GYarXior0AttN,
+      components: {
+        // Add your components here
+		blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page',
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: 'us', // optional,  or 'eu' (default)
+      },
+    })
+  ],
 });
